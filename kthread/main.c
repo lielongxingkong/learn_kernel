f50238b752a160592a3698d52e56f08204e08a78
lielongxingkong <zzl_1164@126.com>
2014-03-16 17:41:55 -0400

    spinlock example runtime define and annotate compile time define

--- kthread/main.c
+++ kthread/main.c
@@ -10,7 +10,13 @@ MODULE_LICENSE("GPL");
 MODULE_AUTHOR("zhaozhenlong");
 MODULE_DESCRIPTION("It's a test module.");
 
-static DEFINE_SPINLOCK(threads_lock);
+//static DEFINE_SPINLOCK(threads_lock);
+static spinlock_t threads_lock;
+
+static void threads_spinlock_init(void){
+	spin_lock_init(&threads_lock);
+}
+
 struct our_data{
 	int cnt1;
 	int cnt2;
@@ -55,6 +61,7 @@ static int cleanup_threads(void){
 static __init int minit(void){
 	printk("call %s\n", __FUNCTION__);
 	printk("init mydata: cnt1:%d, cnt2:%d\n", mydata.cnt1, mydata.cnt2);
+	threads_spinlock_init();
 	if(create_threads())
 		goto err;
 	return 0;
